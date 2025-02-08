f7eb37eda7cabe2bef398d0d339bb39910a75585
sachin barali <baralisachin5925@gmail.com>
2025-02-08 09:22:20 +0545

    book and detail page added

--- userpage/urls.py
+++ userpage/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage,name='homepage'),
-    path('/books',views.bookpage,name='bookpage'),
+    path('books/',views.bookpage,name='bookpage'),
+    path('books/<int:book_id>/',views.book_details,name='book-details'),
 ]
--- userpage/views.py
+++ userpage/views.py
@@ -4,10 +4,27 @@ from books.models import Book
 # Create your views here.
 
 def homepage(request):
-    books = Book.objects.all().order_by('-id')[:4]
+    books = Book.objects.all().order_by('-id')[:8]
     return render(request,'client/homepage.html',{
         'books': books
     })
 
 def bookpage(request):
-    return render(request,'client/bookpage.html')
+    books = Book.objects.all().order_by('-created_at')
+
+    return render(request,'client/bookpage.html',{'books':books})
+
+def book_details(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request,'client/bookdetail.html',{'book':book})
+
+
+# add to cart
+# delete from cart 
+# order
+# checout
+# myorder 
+# marks as deliver(deliver , status deliver)
+# payment method 
+# logout features
+# restriction
